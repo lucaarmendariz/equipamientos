86b1ee2b94a3bc12407deedddf0ff815a8d103a5
Luca <larmendarizl23@fpsanturtzilh.eus>
2025-10-07 09:58:45 +0200

    Datu basearen konexio froga

--- backend/conexion.php
+++ backend/conexion.php
@@ -2,11 +2,12 @@
 $host = 'localhost';
 $db = 'erronka';
 $user = 'root';
-$pass = '';
+$pass = 'ikasle123';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Kaixo konexio ona";
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
